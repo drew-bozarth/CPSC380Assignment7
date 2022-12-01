2119b6f4f05b5b01e139b292cc2b6f69b83573ac
Drew-bozarth <dbozarth@chapman.edu>
2022-11-30 17:16:11 -0800

    updating code in algos

--- diskscheduler.c
+++ diskscheduler.c
@@ -1,11 +1,18 @@
+/*
+Drew Bozarth | Romtin Rezvani
+2373658 | 2341841
+dbozarth@chapman.edu | rrezvani@chapman.edu
+CPSC 380-01
+Assignment 7 - Disk Scheduling Algorithms - diskscheduler.c
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #define numCylinders 5000
 #define numRequests 1000
 
-int arr[numRequests];
-int testArray[numRequests];
+int inputArray[numRequests];
 
 int startIndex = 0;
 
@@ -19,149 +26,147 @@ after that algorithm to be used in SCAN algorithm.
 int* sort_array() {
 	int i = 0; 
 	int j = 0;
-	int temp = 0;
+	int swap = 0;
 
 	for (i = 0; i < numRequests; ++i) {
        	for (j = i + 1; j < numRequests; ++j) {
-            if (arr[i] > arr[j]) {
-                temp =  arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+            if (inputArray[i] > inputArray[j]) {
+                swap =  inputArray[i];
+                inputArray[i] = inputArray[j];
+                inputArray[j] = swap;
             }
         }
     }
-    return arr;
+    return inputArray;
 }
-
 
 /* First-Come-First-Serve (fcfs) starts from the index after the starting
 index and continually adds the headmovement from the starting index in 
 order recieved. If at end of array, startIndex from index zero and continually 
 add until starting index */
-int fcfs(int *arr) {
+int fcfs(int *inputArray) {
 
-	int i = 0, head_movement = 0, this_start = arr[startIndex];
+	int i = 0;
+	headMovement = 0;
+	newStart = inputArray[startIndex];
 
     for(i = startIndex; i < numRequests; i++) {
 
-    	head_movement += abs(arr[i] - this_start);
+    	headMovement += abs(inputArray[i] - newStart);
     }
 
     for(i = 0; i < startIndex; i++) {
 
-    	head_movement += abs(this_start - arr[i]);
+    	headMovement += abs(newStart - inputArray[i]);
     }
         
-    return head_movement;
+    return headMovement;
 }
 
-/* Shortest-Seek-Time-First (SSTF) takes the current head position, and
-adds the position closest to the current head. This new position now becomes
-the head, then this system repeats. 
-First we sort the array. Then We have counters for above and below startIndex 
-index that we decrement if used. Once these equal to REQUEST-2 (excluding 
-startIndex index) we exit. */
-int sstf(int * arr) {
+/* Shortest Seek Time First (SSTF) algorithm has the current position and
+adds the position closest to curr. The new position becomes the head and this
+will cyclically repeat. First the array is sorted. We made counters for the indexes
+above and below startIndex that are decremented if used. When the counters are equal
+to numRequests - 2 it exits. */
+int sstf(int * inputArray) {
 
-	arr = sort_array();
+	inputArray = sort_array();
 
-	int small_i = startIndex-1, large_i = startIndex+1;
-	int small_diff = 0, large_diff = 0;
-	int head_movement = 0, total = numRequests-2, new_head = startIndex, head_value = arr[startIndex];
+	int prevIndex = startIndex-1;
+	int nextIndex = startIndex+1;
+	int prevDiff = 0;
+	int nextDiff = 0;
+	int headMovement = 0;
+	int total = numRequests-2;
+	int newHead = startIndex;
+	int headVal = inputArray[startIndex];
 	
 	while(total >= 0) {
+		prevDiff = abs(inputArray[newHead] - inputArray[prevIndex]);
+		nextDiff = abs(inputArray[nextIndex] - inputArray[newHead]);
 
-		small_diff = abs(arr[new_head] - arr[small_i]);
-		large_diff = abs(arr[large_i] - arr[new_head]);
-
-		if(small_diff < large_diff) {
-
-			head_movement += small_diff;
-			new_head = small_i;
-			small_i--;
-			
+		if(prevDiff < nextDiff) {
+			headMovementment += prevDiff;
+			newHead = prevIndex;
+			prevIndex--;
 		} else {
-
-			head_movement += large_diff;
-			new_head = large_i;
-			large_i++;
+			headMovementment += nextDiff;
+			newHead = nextIndex;
+			nextIndex++;
 		}
-
 		total--;
 	}
 
-	return head_movement;
+	return headMovementment;
 }
 
 /* SCAN - array is already sorted from sstf. SCAN starts from one left of startIndex, 
 and continually goes down to zero (if included in randome array or not). Then 
 starts at one higher than startIndex and continually goes up to highest value (not 5000) */
-int scan(int * ranArray) {
+int scan(int * inputArray) {
+	int i = 0;
+	int currVal = 0;
+	int tempVal = inputArray[startIndex];
+	int diff = 0;
 
-	int i = 0, curr_val = 0, sav_val = arr[startIndex], difference = 0;
-	int head_movement = 0, curr_i = 0;
+	int headMovement = 0;
+	int currIndex = 0;
 
 	for(i = startIndex-1; i >= 0; --i) {
-
-		curr_val = arr[i];
-		difference = abs(sav_val - curr_val);
-		head_movement += difference;
-		sav_val = curr_val;
-
+		currVal = inputArray[i];
+		diff = abs(tempVal - currVal);
+		headMovement += diff;
+		tempVal = currVal;
 	}
 
 	/* used to subtract value from zero, or just add same value */
-	head_movement += sav_val;
-	sav_val = 0;
+	headMovement += tempVal;
+	tempVal = 0;
 
 	for(i = startIndex+1; i < numRequests; i++) {
-
-		curr_val = arr[i];
-		difference = abs(curr_val - sav_val);
-		head_movement += difference;
-		sav_val = curr_val;
-
+		currVal = inputArray[i];
+		diff = abs(currVal - tempVal);
+		headMovement += diff;
+		tempVal = currVal;
 	}
 
-	return head_movement;
-
+	return headMovement;
 }
 
 /* Circular Scan (C-SCAN) - startIndex at startIndex index, increase to upper boundary 
 (even if no value at boundary), save boundary value, go to startIndex boundary 
 (zero value) increase till last value before startIndex value */
-int cscan(int * ranArray) {
+int cscan(int * inputArray) {
 
-	int i = 0, curr_val = 0, sav_val = arr[startIndex], difference = 0;
-	int head_movement = 0, curr_i = 0, upper_bound = 4999;
+	int i = 0;
+	int currVal = 0;
+	int tempVal = inputArray[startIndex];
+	int diff = 0;
+	int headMovement = 0;
+	int currIndex = 0;
+	int upperBound = 4999;
 
 	for(i = startIndex+1; i < numRequests; i++) {
-
-		curr_val = arr[i];
-		difference = abs(sav_val - curr_val);
-		head_movement += difference;
-		sav_val = curr_val;
-
+		currVal = inputArray[i];
+		diff = abs(tempVal - currVal);
+		headMovement += diff;
+		tempVal = currVal;
 	}
 
 	/* add last val - upper bound, go to and add zero bounday (4999)*/
-	head_movement += upper_bound - sav_val;
-	sav_val = 0;
-	head_movement += 4999;
+	headMovement += upperBound - tempVal;
+	tempVal = 0;
+	headMovement += 4999;
 
 	for(i = 0; i < startIndex; i++) {
-
-		curr_val = arr[i];
-		difference = abs(curr_val - sav_val);
-		head_movement += difference;
-		sav_val = curr_val;
-
+		currVal = inputArray[i];
+		diff = abs(currVal - tempVal);
+		headMovement += diff;
+		tempVal = currVal;
 	}
 
-	return head_movement;
+	return headMovement;
 }
-
-
 
 
 
@@ -184,16 +189,16 @@ int main (int argc, char *argv[]) {
 	}
 
 	for (int i = 0; i < numRequests; ++i) {
-		fscanf(ptr, "%d", &arr[i]); 
+		fscanf(ptr, "%d", &inputArray[i]); 
 	}
 
 	printf("Start index: %d\n", startIndex);
-	printf("startIndex value: %d\n", arr[startIndex]);
+	printf("startIndex value: %d\n", inputArray[startIndex]);
 
-	printf("Total amount of FCFS head movements: %d\n", fcfs(arr));
-	printf("Total amount of SSTF head movements: %d\n", sstf(arr));
-	printf("Total amount of SCAN head movements: %d\n", scan(arr));
-	printf("Total amount of CSCAN head movements: %d\n", cscan(arr));
+	printf("Total amount of FCFS head movements: %d\n", fcfs(inputArray));
+	printf("Total amount of SSTF head movements: %d\n", sstf(inputArray));
+	printf("Total amount of SCAN head movements: %d\n", scan(inputArray));
+	printf("Total amount of CSCAN head movements: %d\n", cscan(inputArray));
 
 	fclose(ptr);
 	return 0;
